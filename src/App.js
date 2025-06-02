d211bffd29f91f4baea237a50e69a6357e7e9f17
Cem Tosun <cemtosun@mail.com>
2025-06-02 10:53:01 +0300

    02 Jun 2025

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './assets/thecrownless_logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <img src={logo} alt="The Crownless Logo" className="logo" />
+
+      <p className="description">
+        <strong>The Crownless</strong> is a next-generation Medieval MMORPG bringing together
+        classic old-school game excitement and modern interfaces. Dive into
+        rich lore, skill-based combat, and a vibrant player-driven world.
+      </p>
+
+      <p className="status">⚔️ Pre-Sale will start on the 1st of September 2025</p>
+
     </div>
   );
 }
